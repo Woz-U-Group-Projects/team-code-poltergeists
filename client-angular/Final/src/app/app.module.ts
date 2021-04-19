bf29f87254cd3335d8bb762aaac106c3c0d04a4c
antargt <aragonanthonyj@gmail.com>
2021-04-19 03:35:54 -0700

    removed uneeded packages

--- client-angular/Final/src/app/app.module.ts
+++ client-angular/Final/src/app/app.module.ts
@@ -5,20 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
 
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatCardModule } from '@angular/material/card';
-import { MatMenuModule } from '@angular/material/menu';
 
-import { FlexLayoutModule } from '@angular/flex-layout';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
 import { AuthModule } from './auth/auth.module';
 import { SharedModule } from './shared/shared.module';
 import { HttpClientModule } from '@angular/common/http';
@@ -47,8 +35,6 @@ import { DsPumpDetailsComponent } from './auth/components/dsPump-details/dsPump-
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    LayoutModule,
-    FlexLayoutModule, 
     AuthModule,
     SharedModule,
     HttpClientModule,
